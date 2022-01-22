e72908fc557285be6fb257844734d7ffc2cee8a9
valeriadayanna <valeria.torres2017@uteq.edu.ec>
2022-01-21 20:09:34 -0500

    Main

--- app/src/main/java/com/example/downloadmanager/MainActivity.java
+++ app/src/main/java/com/example/downloadmanager/MainActivity.java
@@ -1,8 +1,21 @@
 package com.example.downloadmanager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.app.DownloadManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +23,73 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayList<String> permisos = new ArrayList<String>();
+        permisos.add(Manifest.permission.CAMERA);
+        permisos.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        permisos.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+        permisos.add(Manifest.permission.WRITE_CALENDAR);
+        getPermission(permisos);
+    }
+    public void getPermission(ArrayList<String> permisosSolicitados){
+
+        ArrayList<String> listPermisosNOAprob = getPermisosNoAprobados(permisosSolicitados);
+        if (listPermisosNOAprob.size()>0)
+            if (Build.VERSION.SDK_INT >= 23)
+                requestPermissions(listPermisosNOAprob.toArray(new String[listPermisosNOAprob.size()]), 1);
+
+    }
+    public ArrayList<String> getPermisosNoAprobados(ArrayList<String>  listaPermisos) {
+        ArrayList<String> list = new ArrayList<String>();
+        for(String permiso: listaPermisos) {
+            if (Build.VERSION.SDK_INT >= 23)
+                if(checkSelfPermission(permiso) != PackageManager.PERMISSION_GRANTED)
+                    list.add(permiso);
+
+        }
+        return list;
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        String s="";
+        if(requestCode==1)    {
+            for(int i =0; i<permissions.length;i++) {
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED)
+                    s=s + "OK " + permissions[i] + "\n";
+                else
+                    s=s + "NO  " + permissions[i] + "\n";
+            }
+            Toast.makeText(this.getApplicationContext(), s, Toast.LENGTH_LONG).show();
+        }
+    }
+    public void MostrarDescargas(View view){
+
+        Intent intent = new Intent();
+        intent.setAction(DownloadManager.ACTION_VIEW_DOWNLOADS);
+        startActivity(intent);
+
+    }
+    public void BajarDoc(View view){
+
+
+        String url = "https://revistas.uteq.edu.ec/formatos/instrucciones%20para%20autores.pdf";
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+        request.setDescription("PDF");
+        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE | DownloadManager.Request.NETWORK_WIFI);
+        request.setTitle("Pdf");
+        request.setVisibleInDownloadsUi(true);
+        //if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+        //request.allowScanningByMediaScanner();
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        //}
+        request.setDestinationInExternalFilesDir(this.getApplicationContext(), Environment.DIRECTORY_DOWNLOADS, "file.pdf");
+        //request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "filedownload.pdf");
+        DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+        try {
+            manager.enqueue(request);
+        } catch (Exception e) {
+            Toast.makeText(this.getApplicationContext(),"Error: "  + e.getMessage(),Toast.LENGTH_LONG).show();
+        }
+
     }
 }
